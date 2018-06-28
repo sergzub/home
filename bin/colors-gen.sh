87c228340217423bf85d68da4d731386ba1c9f33
Sergey Zubkovsky <sergzub@gmail.com>
2018-06-28 16:37:31 +0300

    Added 'colors.shh' basically generated by 'colors-gen.sh' script

--- bin/colors-gen.sh
+++ bin/colors-gen.sh
@@ -28,35 +28,30 @@ SetColorVarCode()
     PrintHexCodes "${1}"
 }
 
-# Commented do not work in PuTTy at least
+# --------------------------------------------
+SetColorVarTPut SGR0                sgr0
 
-SetColorVarTPut COLOR_RESET         sgr0
-SetColorVarTPut TEXT_RESET          sgr0
+SetColorVarTPut BLACK               setaf 0
+SetColorVarTPut RED                 setaf 1
+SetColorVarTPut GREEN               setaf 2
+SetColorVarTPut YELLOW              setaf 3
+SetColorVarTPut BLUE                setaf 4
+SetColorVarTPut MAGENTA             setaf 5
+SetColorVarTPut CYAN                setaf 6
+SetColorVarTPut WHITE               setaf 7
 
-SetColorVarTPut COLOR_BLACK         setaf 0
-SetColorVarTPut COLOR_RED           setaf 1
-SetColorVarTPut COLOR_GREEN         setaf 2
-SetColorVarTPut COLOR_YELLOW        setaf 3
-SetColorVarTPut COLOR_BLUE          setaf 4
-SetColorVarTPut COLOR_MAGENTA       setaf 5
-SetColorVarTPut COLOR_CYAN          setaf 6
-SetColorVarTPut COLOR_WHITE         setaf 7
-
-SetColorVarTPut COLOR_BLACK_BG      setab 0
-SetColorVarTPut COLOR_RED_BG        setab 1
-SetColorVarTPut COLOR_GREEN_BG      setab 2
-SetColorVarTPut COLOR_YELLOW_BG     setab 3
-SetColorVarTPut COLOR_BLUE_BG       setab 4
-SetColorVarTPut COLOR_MAGENTA_BG    setab 5
-SetColorVarTPut COLOR_CYAN_BG       setab 6
-SetColorVarTPut COLOR_WHITE_BG      setab 7
+SetColorVarTPut BLACK_BG            setab 0
+SetColorVarTPut RED_BG              setab 1
+SetColorVarTPut GREEN_BG            setab 2
+SetColorVarTPut YELLOW_BG           setab 3
+SetColorVarTPut BLUE_BG             setab 4
+SetColorVarTPut MAGENTA_BG          setab 5
+SetColorVarTPut CYAN_BG             setab 6
+SetColorVarTPut WHITE_BG            setab 7
 
 SetColorVarTPut TEXT_BOLD           bold
-# SetColorVarTPut TEXT_DIM            dim
 SetColorVarTPut TEXT_BLINK          blink
-SetColorVarTPut TEXT_REV            rev
-# SetColorVarTPut TEXT_HIDDEN         invis
-# SetColorVarTPut TEXT_PROTECTED      prot
+SetColorVarTPut TEXT_REVERSE        rev
 
 SetColorVarTPut TEXT_STANDOUT_ON    smso
 SetColorVarTPut TEXT_STANDOUT_OFF   rmso
@@ -64,31 +59,40 @@ SetColorVarTPut TEXT_STANDOUT_OFF   rmso
 SetColorVarTPut TEXT_UNDERLINED_ON  smul
 SetColorVarTPut TEXT_UNDERLINED_OFF rmul
 
+# Commented below do not work in PuTTy at least
+
+# SetColorVarTPut TEXT_DIM            dim
+# SetColorVarTPut TEXT_HIDDEN         invis
+# SetColorVarTPut TEXT_PROTECTED      prot
+
 # SetColorVarTPut TEXT_ALTERNATE_ON   smacs
 # SetColorVarTPut TEXT_ALTERNATE_OFF  rmacs
 
 # SetColorVarTPut TEXT_ITALIC_ON      sitm
 # SetColorVarTPut TEXT_ITALIC_OFF     ritm
 
-# ----------------------- --------------------
-# # 256 colors (works in PuTTY but no needed now)
+# --------------------------------------------
+# 256 colors (works in PuTTY but no needed now)
+#
 # SetColorVarCode COLOR_GRAY          $'\x1B[38;5;8m'
 # SetColorVarCode COLOR_GRAY_BG       $'\x1B[48;5;8m'
 
-# ----------------- Tests --------------------
-echo "Bright black: ${COLOR_BLACK}${TEXT_BOLD}gray${COLOR_RESET}"
-echo "Bright green: ${COLOR_GREEN}${TEXT_BOLD}lime${TEXT_RESET}"
-echo "Read: ${COLOR_RED}red${COLOR_RESET}"
-echo "Green BG: ${COLOR_GREEN_BG}GREN_BG${COLOR_RESET}"
-echo "Bold: ${TEXT_BOLD}bold${TEXT_RESET}"
-echo "Blink: ${TEXT_BLINK}blink${TEXT_RESET}"
+# --------------------------------------------
+# Tests
+#
+echo "Bright black: ${BLACK}${TEXT_BOLD}gray${SGR0}"
+echo "Bright green: ${GREEN}${TEXT_BOLD}lime${SGR0}"
+echo "Read: ${RED}red${SGR0}"
+echo "Green BG: ${GREEN_BG}GREN_BG${SGR0}"
+echo "Bold: ${TEXT_BOLD}bold${SGR0}"
+echo "Blink: ${TEXT_BLINK}blink${SGR0}"
 echo "Standout: ${TEXT_STANDOUT_ON}standout${TEXT_STANDOUT_OFF}"
-echo "Standout+blue: ${COLOR_BLUE}${TEXT_STANDOUT_ON}standout${TEXT_STANDOUT_OFF} text with blue${TEXT_RESET}"
+echo "Standout+blue: ${BLUE}${TEXT_STANDOUT_ON}standout${TEXT_STANDOUT_OFF} text with blue${SGR0}"
 echo "Underlined: '${TEXT_UNDERLINED_ON}underlined${TEXT_UNDERLINED_OFF}'"
-echo "Reverse video: ${TEXT_REV}rev${TEXT_RESET}"
+echo "Reverse video: ${TEXT_REVERSE}rev${SGR0}"
 
-#echo "Hidden: '${TEXT_HIDDEN}Hidden${TEXT_RESET}'"
-#echo "Dim: ${TEXT_DIM}dim${TEXT_RESET}"
-#echo "Protected: '${TEXT_PROTECTED}protected${TEXT_RESET}'"
+#echo "Hidden: '${TEXT_HIDDEN}Hidden${SGR0}'"
+#echo "Dim: ${TEXT_DIM}dim${SGR0}"
+#echo "Protected: '${TEXT_PROTECTED}protected${SGR0}'"
 #echo "Italic: ${TEXT_ITALIC_ON}italic${TEXT_ITALIC_OFF}"
 #echo "Alternate: ${TEXT_ALTERNATE_ON}alternate${TEXT_ALTERNATE_OFF}"
