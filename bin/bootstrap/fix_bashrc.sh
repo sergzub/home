9317f0ee4734827a553f5d9c8e399de083cea596
Sergey Zubkovsky <sergzub@gmail.com>
2024-03-20 21:37:18 +0300

    Added init_home.sh, fix_bashrc.sh

---
+++ bin/bootstrap/fix_bashrc.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -eu
+
+UncommentAlias()
+{
+    local a="${1}"
+    local bashrc="${HOME}/.bashrc_tst"
+
+    sed -i "s|#alias ${a}|alias ${a}|" ${bashrc} || true
+}
+
+FixPathEnvVar()
+{
+    :
+}
+
+Main()
+{
+    UncommentAlias 'grep'
+    UncommentAlias 'fgrep'
+    UncommentAlias 'egrep'
+
+    UncommentAlias 'll'
+    UncommentAlias 'la'
+    UncommentAlias 'l'
+
+    FixPathEnvVar
+}
+
+Main "$@"
---
+++ bin/bootstrap/init_home.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -eux
+
+Main()
+{
+    local MY_HOME_SSH_DIR="${HOME}/.ssh"
+
+    # Check for files
+    stat "id_rsa.pub"
+    stat "github_id_rsa"
+
+    mkdir -p "${MY_HOME_SSH_DIR}"
+    chmod 700 "${MY_HOME_SSH_DIR}"
+
+    cat "id_rsa.pub" >> "${MY_HOME_SSH_DIR}/authorized_keys"
+    chmod 600 "${MY_HOME_SSH_DIR}/authorized_keys"
+    rm "id_rsa.pub"
+
+    chmod 600 "github_id_rsa"
+    mv -i "github_id_rsa" "${MY_HOME_SSH_DIR}/"
+
+    cat << HereDoc_SSH_CONFIG_APPEND >> "${MY_HOME_SSH_DIR}/config"
+Host github.com
+    PreferredAuthentications publickey
+    IdentityFile /home/${USER}/.ssh/github_id_rsa
+HereDoc_SSH_CONFIG_APPEND
+
+    sudo ${SHELL} -c "cat > /etc/sudoers.d/${USER}" << HereDoc_SUDOERSD_USER
+${USER} ALL=NOPASSWD:ALL
+Defaults:${USER} env_keep += "HOME"
+Defaults:${USER} env_keep += "PATH"
+HereDoc_SUDOERSD_USER
+
+    # sudo apt install git vcsh
+    # vcsh clone git@github.com:sergzub/home.git master
+
+    # sudo apt install mc htop
+}
+
+Main "$@"
